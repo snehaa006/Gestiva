0da9cb4ce7d40978640f2e48de56f4a50aab61bb
Sneha gupta <snehagupta@Snehas-Laptop.local>
2025-06-30 22:45:10 +0530

    Rebuild: correct base path for GitHub Pages

--- gestiva-maternal-compass-85/Frontend/src/main.tsx
+++ gestiva-maternal-compass-85/Frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 
-<BrowserRouter basename="/Gestiva">
-  <App />
-</BrowserRouter>
-
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Gestiva">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
